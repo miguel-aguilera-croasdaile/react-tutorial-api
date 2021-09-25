14ed433069c9b391227af6f6d609c083fcd75381
Miguel Aguilera <miguel.aguilera.croasdaile@gmail.com>
2021-09-25 11:28:48 -0300

    testing another cors gem config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,2 @@
 class ApplicationController < ActionController::Base
-  skip_forgery_protection
-  skip_before_action :verify_authenticity_token
 end
--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,6 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-
 module ReactApiTutorial
   class Application < Rails::Application
     config.generators do |generate|
@@ -16,14 +15,6 @@ module ReactApiTutorial
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
-      end
-    end
-
 
     # Configuration for the application, engines, and railties goes here.
     #

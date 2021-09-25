aaaac296ef79f04ddbf4f1b5b2f57ba6c87e3e62
Miguel Aguilera <miguel.aguilera.croasdaile@gmail.com>
2021-09-25 11:21:42 -0300

    moved cors access permission lines to app.rb

--- config/application.rb
+++ config/application.rb
@@ -6,12 +6,6 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.middleware.insert_before 0, Rack::Cors do
-  allow do
-     origins '*'
-     resource '*', :headers => :any, :methods => [:get, :post, :options]
-   end
-end
 
 module ReactApiTutorial
   class Application < Rails::Application
@@ -22,6 +16,14 @@ module ReactApiTutorial
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+
 
     # Configuration for the application, engines, and railties goes here.
     #

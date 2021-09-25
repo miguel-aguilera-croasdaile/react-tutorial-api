de20557cd18376c7a4a51a69b78b023b87d3c9b2
Miguel Aguilera <miguel.aguilera.croasdaile@gmail.com>
2021-09-25 11:16:36 -0300

    Cross origin problem solved, removing application controller forgery protection to remove need for auth token from form

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,7 +6,3 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
-
-# //Here you are specifying from what url's you are accepting fetch calls from. in our case you will want to switch example.com to * to allow all websites.
-# example: origins "*"
-# //Here you can also limit the methods that you will be accepting.

18dd34eb6b06e23e549c55c0306cd41f21b4f9f6
Miguel Aguilera <miguel.aguilera.croasdaile@gmail.com>
2021-09-26 11:10:44 -0300

    cors woes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,2 @@
 class ApplicationController < ActionController::Base
-  skip_before_action :verify_authenticity_token
-  skip_before_filter :verify_authenticity_token, if: :json_request?
-
-  def json_request?
-    request.format.json?
-  end
 end
